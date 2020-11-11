42c63a434898797fdb314ef2c931b98d894aeb6f
noinguyen <huunoidq@gmail.com>
2020-11-11 10:29:19 +0700

    update challeng a

--- challenge_a.py
+++ challenge_a.py
@@ -6,11 +6,15 @@ from generator import generate_string
 
 
 def main():
+    str_types = ['alphabetical', 'int', 'real', 'alphanumeric']
+    choice = random.choice(str_types)
+    generated_str = generate_string(default_type=choice)
+    with open('strings.txt', mode='w+') as f:
+        f.write(generated_str.__str__())
     while True:
         file_size = get_file_size(filename='strings.txt')
         if abs(file_size - 10) <= 10e-4:
-            break
-        str_types = ['alphabetical', 'int', 'real', 'alphanumeric']
+            break        
         choice = random.choice(str_types)
         generated_str = generate_string(default_type=choice)
         with open('strings.txt', mode='a+') as f:
